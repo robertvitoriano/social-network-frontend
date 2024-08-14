3ad52663825ddef4358371a6dc99b17ef7ee3e94
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-08-14 16:44:12 +0900

    finish login oauth logic

--- src/lib/google-auth-keys.ts
+++ src/lib/google-auth-keys.ts
@@ -1,13 +1,7 @@
-type GOOGLE_AUTH_KEYS =
-  | "clientId"
-  | "clientSecret"
-  | "endpoint"
-  | "redirectURI"
-  | "scopes";
+type GOOGLE_AUTH_KEYS = "clientId" | "endpoint" | "redirectURI" | "scopes";
 
 export const oauthGoogle: Record<GOOGLE_AUTH_KEYS, string> = {
   clientId: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID || "",
-  clientSecret: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_SECRET || "",
   endpoint: "https://accounts.google.com/o/oauth2/v2/auth",
   redirectURI: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URI || "",
   scopes:
