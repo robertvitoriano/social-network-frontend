bfafb3dc62a00d36f3c49ede9ef6b3f1f5e66288
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-08-15 18:58:12 +0900

    toggle friends sidebar

--- src/components/MessagesSidebar.tsx
+++ src/components/MessagesSidebar.tsx
@@ -5,11 +5,13 @@ import { ChatPopOver, Receiver } from "./ChatPopOver";
 import classNames from "classnames";
 import socket from "../api/websocket-service";
 import { EventType } from "@/enums/websocket-events";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export const MessagesSideBar = () => {
   const [userFriends, setUserFriends] = useState<Receiver[]>([]);
   const [showChat, setShowChat] = useState(false);
   const [friendInCurrentChat, setFriendInCurrentChat] = useState<Receiver>();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   useEffect(() => {
     loadUserFriends();
@@ -53,40 +55,58 @@ export const MessagesSideBar = () => {
   function handleChatClose() {
     setShowChat(false);
   }
+  function toggleSidebar() {
+    setIsSidebarOpen(!isSidebarOpen);
+  }
   return (
     <>
-      <div className="bg-primary w-72 h-screen absolute right-0 text-white">
-        {userFriends.map((friend: Receiver) => (
-          <div
-            className="w-full border-2 flex gap-4 flex-col border-white p-4 cursor-pointer"
-            onClick={() => handleChatOpen(friend)}
-            key={friend.id}
-          >
-            <div className="flex gap-4 items-center">
-              <img className="rounded-full h-12 w-12" src={friend.avatar} />
+      <div
+        className={classNames(
+          "bg-primary h-screen absolute text-white transition-transform duration-300",
+          {
+            "right-0 w-72": isSidebarOpen,
+            "-right-64 w-72": !isSidebarOpen,
+          }
+        )}
+      >
+        <div
+          className="p-2 cursor-pointer hover:bg-black"
+          onClick={toggleSidebar}
+        >
+          {isSidebarOpen ? <ChevronRight /> : <ChevronLeft />}
+        </div>
+        {isSidebarOpen &&
+          userFriends.map((friend: Receiver) => (
+            <div
+              className="w-full border-2 flex gap-4 flex-col border-white p-4 cursor-pointer"
+              onClick={() => handleChatOpen(friend)}
+              key={friend.id}
+            >
               <div className="flex gap-4 items-center">
-                <span>{friend.name}</span>
-                <div
-                  className={classNames(
-                    "w-3",
-                    "h-3",
-                    { "bg-green-500": friend.online },
-                    { "bg-secondary": !friend.online },
-                    "rounded-full"
-                  )}
-                ></div>
+                <img className="rounded-full h-12 w-12" src={friend.avatar} />
+                <div className="flex gap-4 items-center">
+                  <span>{friend.name}</span>
+                  <div
+                    className={classNames(
+                      "w-3",
+                      "h-3",
+                      { "bg-green-500": friend.online },
+                      { "bg-secondary": !friend.online },
+                      "rounded-full"
+                    )}
+                  ></div>
+                </div>
               </div>
+              <div>
+                <p>{friend.lastMessage}</p>
+              </div>
+              <span className="text-right">
+                {new Date(friend.lastMessageCreatedAt).toLocaleDateString() +
+                  " - " +
+                  new Date(friend.lastMessageCreatedAt).toLocaleTimeString()}
+              </span>
             </div>
-            <div>
-              <p>{friend.lastMessage}</p>
-            </div>
-            <span className="text-right">
-              {new Date(friend.lastMessageCreatedAt).toLocaleDateString() +
-                " - " +
-                new Date(friend.lastMessageCreatedAt).toLocaleTimeString()}
-            </span>
-          </div>
-        ))}
+          ))}
       </div>
       {friendInCurrentChat && showChat && (
         <ChatPopOver receiver={friendInCurrentChat} onClose={handleChatClose} />
