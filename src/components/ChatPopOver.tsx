30694567f7bab6cdff29b1dca082b4e0f630adf3
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-08-15 13:04:57 +0900

    emit message in order to display correctly

--- src/components/ChatPopOver.tsx
+++ src/components/ChatPopOver.tsx
@@ -105,6 +105,7 @@ export function ChatPopOver({ onClose, receiver }: ChatPopOverProps) {
     socket.emit(EventType.MESSAGE_SENT, {
       ...newMessage,
       receiverId: receiver.id,
+      isFromUser: false,
     });
     setCurrentMessageContent("");
     scrollToBottom();
