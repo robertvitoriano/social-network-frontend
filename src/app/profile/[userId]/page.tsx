c482e19fb9cf78d4277097c6c9a29e3f613f2a62
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-08-26 13:33:36 +0900

    improve  profile positioning

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -31,25 +31,53 @@ const UserProfile = () => {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <div className="bg-secondary text-white flex flex-1">
+    <div className="bg-secondary text-white flex flex-1 flex-col">
       {user ? (
-        <div
-          className="relative border-b border-black flex flex-1 h-52 lg:h-80"
-          style={{
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundImage:
-              'URL("https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630")',
-          }}
-        >
-          <div className="p-8 flex flex-col gap-4 absolute w-full items-center lg:flex-row lg:-bottom-[280px]">
-            <img
-              src={user.avatar}
-              className="h-60 w-60 rounded-full border-solid border-8 border-white"
-            />
-            <h2 className="text-4xl">{user.name}</h2>
+        <>
+          <div
+            className="relative border-b border-black flex h-52 lg:h-80"
+            style={{
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundImage:
+                'URL("https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630")',
+            }}
+          >
+            <div className="flex flex-col gap-4 absolute w-full items-start lg:flex-row  -bottom-[60px] lg:-bottom-[280px] p-4 pl-2">
+              <img
+                src={user.avatar}
+                className="h-36 w-36 rounded-full border-solid border-4 border-white"
+              />
+              <div className="flex-col gap-1 hidden lg:flex">
+                <h2 className="text-2xl">{user.name}</h2>
+                <h2 className="text-base">
+                  <span className="font-bold">1.6 th </span> friends
+                </h2>
+              </div>
+            </div>
           </div>
-        </div>
+          <div className="flex-col gap-1 flex mt-10">
+            <h2 className="text-2xl">{user.name}</h2>
+            <h2 className="text-base">
+              <span className="font-bold">1.6 th </span> friends
+            </h2>
+          </div>
+          <div className="p-4 pr-12 flex flex-col gap-4">
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+            <div className="h-32 w-full bg-primary"></div>
+          </div>
+        </>
       ) : (
         <p>User not found</p>
       )}
